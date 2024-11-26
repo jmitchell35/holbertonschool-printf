b0c6686d6a85683c9881fb73bd258b53cac80bdc
Damien353 <10034@holbertonstudents.com>
2024-11-26 11:20:22 +0000

    integration fonction de matching dans la fonction printf

--- _printf.c
+++ _printf.c
@@ -6,36 +6,26 @@
  */
 int _printf(const char *format, ...)
 {
-	int format_iter = 0, length = 0, print_ops_iter = 0;
-	print_t print_ops = {
-		{"c", print_char},
-		{"s", print_str},
-		{NULL, NULL}
-	};
+	int format_iter = 0, length = 0;
 	va_list args;
+	int (*ptr_to_func(char *));
 
 	va_start(args, format);
 	while (format[format_iter])
 	{
-	if (format[format_iter] == '%')
+	if (format[format_iter] == '%' && format[format_iter + 1])
 	{
-		for (print_ops_iter = 0; print_ops_iter < 2; print_ops_iter++)
-		{
-			if (format[format_iter + 1] == print_ops[print_ops_iter].specifier)
-			{
-			print_ops[print_ops_iter].ptr_to_print_func(args, void *);
-			format_iter++;
-			}
-		}
-	_putchar(format[format_iter]);
+	ptr_to_func = get_print_op(format[format_iter + 1]);
+	if (ptr_to_func != NULL)
+	{
+		length += ptr_to_func(args, char *);
+		format_iter++;
+	}
+	length += _putchar(format[format_iter]);
 	format_iter++;
-	length++;
 	}
 	else
-	{
-		_putchar(format[format_iter]);
-		length++;
-	}
+		length += _putchar(format[format_iter]);
 	format_iter++;
 	}
 	va_end(args);
