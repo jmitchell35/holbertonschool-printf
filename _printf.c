5432dab75adc394521c95c32c70f1dd43e61f6ab
Damien353 <10034@holbertonstudents.com>
2024-11-26 14:43:35 +0000

    Testing fixing matching types problems

--- _printf.c
+++ _printf.c
@@ -9,23 +9,24 @@ int _printf(const char *format, ...)
 	int format_iter = 0, length = 0;
 	va_list args;
 	int (*ptr_to_func(char *));
+	char *ptr_to_format = (char *)format;
 
 	va_start(args, format);
-	while (format[format_iter])
+	while (ptr_to_format[format_iter])
 	{
-	if (format[format_iter] == '%' && format[format_iter + 1])
+	if (ptr_to_format[format_iter] == '%' && ptr_to_format[format_iter + 1])
 	{
-	ptr_to_func = get_print_op(format[format_iter + 1]);
+	ptr_to_func = get_print_op(&ptr_to_format[format_iter + 1]);
 	if (ptr_to_func != NULL)
 	{
 		length += ptr_to_func(args, char *);
 		format_iter++;
 	}
-	length += _putchar(format[format_iter]);
+	length += _putchar(ptr_to_format[format_iter]);
 	format_iter++;
 	}
 	else
-		length += _putchar(format[format_iter]);
+		length += _putchar(ptr_to_format[format_iter]);
 	format_iter++;
 	}
 	va_end(args);
