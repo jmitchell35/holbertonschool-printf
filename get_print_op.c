42e24dfe2a9b57ba5c02dd1a373fe8a875069fb9
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-26 14:51:34 +0100

    Correcting types after modification of _putchar

--- get_print_op.c
+++ get_print_op.c
@@ -7,7 +7,7 @@
   */
 int (*get_print_op(char *s))(char *)
 {
-	print_t print_ops = {
+	print_t print_ops[] = {
 		{"c", _putchar},
 		{"s", print_str},
 		{NULL, NULL}
@@ -16,7 +16,7 @@ int (*get_print_op(char *s))(char *)
 
 	for (print_ops_iter = 0; print_ops_iter < 2; print_ops_iter++)
 		{
-			if (*s == print_ops[print_ops_iter].specifier)
+			if (*s == *print_ops[print_ops_iter].specifier)
 			{
 			return (print_ops[print_ops_iter].ptr_to_print_func);
 			}
