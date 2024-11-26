ad61333499c759e7a581d41fda7815138bb64017
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-26 11:17:38 +0100

    Adding matching function to repo

---
+++ get_print_op.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * get_print_op - matches specifier with corresponding printing function
+  * @s: ptr to str (char *)
+  * Return: ptr_to_func
+  */
+int (*get_print_op(char *s)(char *))
+{
+	print_t print_ops = {
+		{"c", print_char},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+	int print_ops_iter;
+
+	for (print_ops_iter = 0; print_ops_iter < 2; print_ops_iter++)
+		{
+			if (*s == print_ops[print_ops_iter].specifier)
+			{
+			return (print_ops[print_ops_iter].ptr_to_print_func);
+			}
+		}
+	return (NULL);
+}
