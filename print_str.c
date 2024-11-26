21b16c1274e572cf63fd231766f414acb5cc76d1
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-26 12:54:21 +0100

    Adding print_str function definition in separate file

---
+++ print_str.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* Prints a string, to stdout */
+
+/**
+* print_str - prints string to std output
+*
+* @str: ptr to string
+*
+* Return: number of bytes printed
+*/
+
+int print_str(char *str)
+{
+	int length = 0;
+
+	while (*str)
+	{
+		length += _putchar(*str);
+		str++;
+	}
+	return (length);
+}
