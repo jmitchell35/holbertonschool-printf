c7d580665807d2d367e27bdce53de4d21ade6fbe
Damien353 <10034@holbertonstudents.com>
2024-11-27 07:45:13 +0000

    Correcting get_print_op

--- get_print_op.c
+++ get_print_op.c
@@ -2,13 +2,13 @@
 
 /**
   * get_print_op - matches specifier with corresponding printing function
-  * @s: ptr to str (char *)
+  * @s: character, specifier passed to function
   * Return: ptr_to_func
   */
-int (*get_print_op(char *s))(char *)
+int (*get_print_op(char s))(va_list)
 {
 	print_t print_ops[] = {
-		{"c", _putchar},
+		{"c", print_char},
 		{"s", print_str},
 		{NULL, NULL}
 	};
@@ -16,7 +16,7 @@ int (*get_print_op(char *s))(char *)
 
 	for (print_ops_iter = 0; print_ops_iter < 2; print_ops_iter++)
 		{
-			if (*s == *print_ops[print_ops_iter].specifier)
+			if (s == *print_ops[print_ops_iter].specifier)
 			{
 			return (print_ops[print_ops_iter].ptr_to_print_func);
 			}
