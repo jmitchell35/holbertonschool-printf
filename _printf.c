56dc0b8b27ef6f832a1772d1cde5125eea43a5da
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-27 10:26:49 +0100

    Fixed loops and conditions, mathcing prints with std lib printf

--- _printf.c
+++ _printf.c
@@ -13,20 +13,23 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format[format_iter])
 	{
-	if (format[format_iter] == '%' && format[format_iter + 1])
-	{
-	ptr_to_func = get_print_op(format[format_iter + 1]);
-	if (ptr_to_func != NULL)
-	{
-		printed_length += ptr_to_func(args);
+		if (format[format_iter] == '%' && format[format_iter + 1])
+		{
+			ptr_to_func = get_print_op(format[format_iter + 1]);
+			if (ptr_to_func != NULL)
+			{
+			printed_length += ptr_to_func(args);
+			format_iter++;
+			}
+			else
+			{
+			printed_length += _putchar(format[format_iter]);
+			}
+			
+		}
+		else
+			printed_length += _putchar(format[format_iter]);
 		format_iter++;
-	}
-	printed_length += _putchar(format[format_iter]);
-	format_iter++;
-	}
-	else
-		printed_length += _putchar(format[format_iter]);
-	format_iter++;
 	}
 	va_end(args);
 	return (printed_length);
