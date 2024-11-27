3e8e22639828548d5ae57ac3909a198c14672b1e
Damien353 <10034@holbertonstudents.com>
2024-11-27 07:48:28 +0000

    Correcting _printf.c to match prototypes

--- _printf.c
+++ _printf.c
@@ -8,25 +8,24 @@ int _printf(const char *format, ...)
 {
 	int format_iter = 0, length = 0;
 	va_list args;
-	int (*ptr_to_func(char *));
-	char *ptr_to_format = (char *)format;
+	int (*ptr_to_func(va_list));
 
 	va_start(args, format);
-	while (ptr_to_format[format_iter])
+	while (format[format_iter])
 	{
-	if (ptr_to_format[format_iter] == '%' && ptr_to_format[format_iter + 1])
+	if (format[format_iter] == '%' && format[format_iter + 1])
 	{
-	ptr_to_func = get_print_op(&ptr_to_format[format_iter + 1]);
+	ptr_to_func = get_print_op(format[format_iter + 1]);
 	if (ptr_to_func != NULL)
 	{
-		length += ptr_to_func(args, char *);
+		length += ptr_to_func(args);
 		format_iter++;
 	}
-	length += _putchar(ptr_to_format[format_iter]);
+	length += _putchar(format[format_iter]);
 	format_iter++;
 	}
 	else
-		length += _putchar(ptr_to_format[format_iter]);
+		length += _putchar(format[format_iter]);
 	format_iter++;
 	}
 	va_end(args);
