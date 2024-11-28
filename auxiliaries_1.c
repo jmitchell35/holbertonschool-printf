86155654ef887464afd727f84051882658f12563
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-28 11:00:52 +0100

    Trying fixing last checker fail

--- auxiliaries_1.c
+++ auxiliaries_1.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+  * get_print_op - matches specifier with corresponding printing function
+  * @s: character, specifier passed to function
+  * Return: ptr_to_func
+  */
+int (*get_print_op(char s))(va_list)
+{
+	print_t print_ops[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{NULL, NULL}
+		};
+	int print_ops_iter;
+
+	for (print_ops_iter = 0; print_ops_iter < 2; print_ops_iter++)
+	{
+		if (s == *print_ops[print_ops_iter].specifier)
+		{
+			return (print_ops[print_ops_iter].ptr_to_print_func);
+		}
+	}
+	return (NULL);
+}
+/**
   * handle_specifier - auxiliary func, handles specifier, shortens code
   * @format: const ptr to format string passed to printf
   * @format_iter: int, index of specifier flag
@@ -38,6 +61,8 @@ int handle_specifier(const char *format, int *format_iter, va_list args)
 			(*format_iter) += 2; /*Skipping both*/
 		}
 	}
+	else if (*format_iter == 0)
+		error_flag = -1;
 	else
 	{
 		error_flag = _putchar('%');
--- test/0-main.c
+++ test/0-main.c
@@ -7,19 +7,93 @@
  */
 int main(void)
 {
-	int len;
-	int len2;
-	char variable[4] = "test";
-	char variable2 = 'A';
-	char variable3 = 65;
+	int len, len2, i;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	printf("len: %d | len2: %d\n", len, len2);
-	_printf("Je fais un %s\n", variable);
-	_printf("%c\n", variable2);
-	_printf("%c\n", variable3);
-	_printf("Je fais un _printf %\n");
-	printf("Je fais un printf %\n");
+	len = _printf("%c\n", '!');
+	len2 = printf("%c\n", '!');
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%s\n", "Hello, world !");
+	len2 = printf("%s\n", "Hello, World !");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("");
+	len2 = printf("");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%c%c%c%c%c\n", 'H', 'e', 'l', 'l', 'o');
+	len2 = printf("%c%c%c%c%c\n", 'H', 'e', 'l', 'l', 'o');
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%s\n", NULL);
+	len2 = printf("%s\n", NULL);
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%s\n", "");
+	len2 = printf("%s\n", "");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%y\n", 123);
+	len2 = printf("%y\n", 123);
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%%\n");
+	len2 = printf("%%\n");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("cc\n");
+	len2 = printf("cc\n");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%c\n");
+	len2 = printf("%c\n");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf(NULL);
+	len2 = printf(NULL);
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("Char: %c, String: %s\n", 'A', "Test");
+	len2 = printf("Char: %c, String: %s\n", 'A', "Test");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("Hello %s, your score is %c.\n", "Alice", 'A');
+	len2 = printf("Hello %s, your score is %c.\n", "Alice", 'A');
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%s\n", "A");
+	len2 = printf("%s\n", "A");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%s\n", "");
+	len2 = printf("%s\n", "");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%s\n", "Hello\0World");
+	len2 = printf("%s\n", "Hello\0World");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%s %s %s %s %s %s\n", "A", "B", "C", "D", "E", "F");
+	len2 = printf("%s %s %s %s %s %s\n", "A", "B", "C", "D", "E", "F");
+	printf("len: %d | len2: %d\n", len, len2);
+	for (i = 0; i < 100; i++)
+	{
+		_printf("Count: %c", 'a');
+		if (i == 99)
+			_printf("\n");
+	}
+	for (i = 0; i < 100; i++)
+	{
+		printf("Count: %c", 'a');
+		if (i == 99)
+			printf("\n");
+	}
+	len = _printf("%r\n", 123);
+	len2 = printf("%r\n", 123);
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("Character:[%c]\n", 'H');
+	len2 = printf("Character:[%c]\n", 'H');
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("String:[%s]\n", "I am a string !");
+	len2 = printf("String:[%s]\n", "I am a string !");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("Unknown:[%r]\n");
+	len2 = printf("Unknown:[%r]\n");
+	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("%");
+	len2 = printf("%");
+	printf("len: %d | len2: %d\n", len, len2);
+
 	return (0);
 }
+
