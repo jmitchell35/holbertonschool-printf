ba3b77848a076e8b7802657fd17b06430dfe1bb1
jmitchell35 <julian.m.mitchell@gmail.com>
2024-11-29 10:16:36 +0100

    Formatting test file

--- test/0-main.c
+++ test/0-main.c
@@ -1,8 +1,6 @@
 #include "main.h"
-
 /**
- * main - Entry point
- *
+ * main - Entry point - test function
  * Return: Always 0
  */
 int main(void)
@@ -93,6 +91,24 @@ int main(void)
 	len = _printf("%");
 	len2 = printf("%");
 	printf("len: %d | len2: %d\n", len, len2);
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
+	printf("%");
+	_printf("%");
 
 	return (0);
 }
